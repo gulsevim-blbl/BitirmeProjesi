68702fad53c79ad4e091952dfa612ecf56f85cc0
Gül Sevim BÜLBÜL <gulsevimblbl@gmail.com>
2023-12-20 18:54:48 +0300

    category page and filters

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -4,9 +4,10 @@ import { useAuth } from "../../context/auth";
 import { GiShoppingBag } from "react-icons/gi";
 import toast from "react-hot-toast";
 import SearchInput from "../Form/SearchInput";
-
+import useCategory from "../../hooks/useCategory";
 const Header = () => {
   const [auth, setAuth] = useAuth();
+  const categories = useCategory();
   const handleLogout = () => {
     setAuth({
       ...auth,
@@ -42,11 +43,33 @@ const Header = () => {
                   Home
                 </NavLink>
               </li>
-              <li className="nav-item">
-                <NavLink to="/category" className="nav-link ">
-                  Category
-                </NavLink>
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to={"/categories"}
+                  data-bs-toggle="dropdown"
+                >
+                  Categories
+                </Link>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link className="dropdown-item" to={"/categories"}>
+                      All Categories
+                    </Link>
+                  </li>
+                  {categories?.map((c) => (
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to={`/category/${c.slug}`}
+                      >
+                        {c.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
               </li>
+
               {!auth?.user ? (
                 <>
                   <li className="nav-item">
